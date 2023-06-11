6b7d095c48b2c1ff517adee669d6472a5077c442
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2023-06-11 15:23:56 +0100

    removed some old code

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import * as vscodetextmate from 'vscode-textmate'
 import * as path from 'path';
 
-let methodNames : string = "abCD";
+let methodNames : string = "";
 
 export async function activate(context: vscode.ExtensionContext) {
     console.log("ACTIVATED");
@@ -25,15 +25,6 @@ export async function activate(context: vscode.ExtensionContext) {
         new MySemanticTokensProvider(),
         new vscode.SemanticTokensLegend(["comment", "method"], [])
     );
-    console.log("semantic highlighting worked out");
-    /*
-    getProvider().then(
-        (provider) => {
-            context.subscriptions.push(provider);
-        }
-    );
-*/
-    console.log("PROVIDER PUSHED")    
 }
 
 async function UpdateMethodNames() {
@@ -49,24 +40,6 @@ async function UpdateMethodNames() {
         }
     }
     console.log("METHODNAMES UPDATED: " + methodNames);
-}
-
-function getProvider() {
-    return new Promise<vscode.Disposable>(
-        (resolve, reject) => {
-            const provider = vscode.languages.registerDocumentSemanticTokensProvider(
-                {
-                    language: "bfp",
-                    scheme: "file"
-                },
-                new MySemanticTokensProvider(),
-                new vscode.SemanticTokensLegend(["comment", "method"], [])
-            );
-            console.log("semantic highlighting worked out");
-            
-            resolve(provider);
-        }
-    );
 }
 
 
@@ -87,38 +60,6 @@ class MySemanticTokensProvider implements vscode.DocumentSemanticTokensProvider 
         }
         
         return tokensBuilder.build();
-    }
-
-    async oldprovideDocumentSemanticTokens(document: vscode.TextDocument) : Promise<vscode.SemanticTokens | null> {
-        const tokensBuilder = new vscode.SemanticTokensBuilder();
-        console.log("PDST BEGAN");
-        for (let i = 0; i < document.lineCount; i++) {
-            const line = document.lineAt(i).text;
-            for (let j = 0; j < line.length; j++) {
-                const tokenType = await this.doesMethodExist(line[j]) ? 1 : 0;
-                if (tokenType == 1) {
-                    
-                }
-                
-            }
-            tokensBuilder.push(i,0,1,1)
-            console.log(document.lineAt(i).text[0]);
-        }
-        return tokensBuilder.build();
-    }
-
-     async doesMethodExist(firstChar: string) : Promise<boolean> {
-        const files = await vscode.workspace.findFiles('**/*.bfp');
-        for (let i = 0; i < files.length; i++) {
-            const workspaceFolders = vscode.workspace.workspaceFolders
-            //if in the same directory, rather than a nested one
-            if (workspaceFolders) {
-                if (path.dirname(files[i].fsPath) == workspaceFolders[0].uri.fsPath) {
-                    return (firstChar == path.basename(files[i].fsPath)[0]);
-                }
-            }
-        }
-        return false;
     }
 }
 
