afc9a4e756b4e9666d7882d84ba825a1ead1166f
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2023-07-04 17:26:29 +0100

    Removed old unused code

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ import { start } from 'repl';
 let methodNames : string = "";
 
 export async function activate(context: vscode.ExtensionContext) {
-    console.log("ACTIVATED");
+    console.log("BFP extension activated");
     UpdateMethodNames();
 
     //for method highlighting
@@ -20,21 +20,6 @@ export async function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(renameFile);
 
     //for errors
-    
-    const provider = vscode.languages.registerDocumentSemanticTokensProvider(
-        {
-            language: "bfp",
-            scheme: "file"
-        },
-        new MySemanticTokensProvider(),
-        new vscode.SemanticTokensLegend(["method"])
-    );
-
-    const languageProvider = new MyLanguageProvider();
-    context.subscriptions.push(provider);
-    //context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider({language: "bfp"}, languageProvider));
-    context.subscriptions.push(languageProvider);
-
     const diagnosticCollection = vscode.languages.createDiagnosticCollection("TESTDIAG");
     vscode.workspace.onDidChangeTextDocument(event => {
         const activeEditor = vscode.window.activeTextEditor;
@@ -246,30 +231,6 @@ function isCodeChar(char : string, extraChars : {debug : boolean, methods : bool
     return codeChars.indexOf(char) > -1
 }
 
-
-function provideCodeActionsold(document : vscode.TextDocument, range : vscode.Range, context : vscode.CodeActionContext, token : vscode.CancellationToken) {
-    const codeActions = [];
-
-    let regex = new RegExp(/[A-Z]/)
-    for (let i = 0; i < document.lineCount; i++) {
-        const result = regex.exec(document.lineAt(i).text)
-        if (result) {
-            const diagnostic = new vscode.Diagnostic(
-                new vscode.Range(
-                    new vscode.Position(i, result.index),
-                    new vscode.Position(i, result.index + 1)
-                ),
-                "TEST ERROR",
-                vscode.DiagnosticSeverity.Error
-            );
-            codeActions.push(diagnostic);
-        }
-    }
-    
-    return codeActions;
-}
-
-
 class MySemanticTokensProvider implements vscode.DocumentSemanticTokensProvider {
     provideDocumentSemanticTokens(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.SemanticTokens> {
         //console.log("pdst began");
@@ -289,48 +250,5 @@ class MySemanticTokensProvider implements vscode.DocumentSemanticTokensProvider 
         return tokensBuilder.build();
     }
 }
-
-class MyLanguageProvider implements vscode.Disposable {
-    private diagnosticCollection: vscode.DiagnosticCollection;
-    constructor() {
-        this.diagnosticCollection = vscode.languages.createDiagnosticCollection();
-    }
-
-    provideDiagnosticCollection(document: vscode.TextDocument) {
-        console.log("diagnostic");
-        this.diagnosticCollection.delete(document.uri);
-
-        const text = document.getText();
-        
-        let lineNo: number = 0;
-        let charNo: number = 0;
-        const lineCount = document.lineCount;
-        
-
-
-        while (!(lineNo == lineCount)) {
-            const lineText = document.lineAt(lineNo).text;
-            
-            //repetition
-            if (lineText[charNo] == "*") {
-                const range = new vscode.Range(lineNo, charNo, lineNo, charNo);
-                const diag: vscode.Diagnostic = new vscode.Diagnostic(range, "TESTING", vscode.DiagnosticSeverity.Error);
-            }
-
-
-            charNo++;
-            if (charNo == lineText.length) {
-                charNo == 0;
-                lineNo++;
-            }
-        }
-    }
-
-    dispose() {
-        this.diagnosticCollection.clear();
-        this.diagnosticCollection.dispose();
-    }
-}
-
 
 enum NumCheckResult {NonInteger, Empty, MissingEndChar, Valid}
